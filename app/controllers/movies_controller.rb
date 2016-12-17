b8838d4c761cc8fd3580923ff1e1848d6a4947f1
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-17 01:04:59 +0000

    Added a basic params search function in the show of the movies controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,11 +1,21 @@
 class MoviesController < ApplicationController
 
   def index
-    render json: Movie.where(genre: include? "War")
+    @movies = Movie.all
+    render json: @movies
   end
 
   def show
-    @movie = Movie.find(params[:id])
+    params = {
+      name: "caeser",
+      genre: ["war", "drama"],
+    }
+    @movie = Movie.all
+    @movie = @movie.where(name: params[:name]).all
+    params[:genre].each do |element|
+      @movie = @movie.where("genre LIKE ?", "%#{element.titleize}").all
+    end
+
     render json: @movie
   end
 
