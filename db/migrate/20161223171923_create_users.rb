fa60ab9702972294c2294351cb85b4033c1372ed
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-23 17:28:13 +0000

    Added a user model and recreated the migration for the users table. Also added 3 user seeds to the seed file

--- db/migrate/20161223171923_create_users.rb
+++ db/migrate/20161223171923_create_users.rb
@@ -5,6 +5,8 @@ class CreateUsers < ActiveRecord::Migration[5.0]
       t.string :email
       t.string :password
       t.string :password_confirmation
+
+      t.timestamps
     end
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161223165054) do
+ActiveRecord::Schema.define(version: 20161223171923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -46,10 +46,12 @@ ActiveRecord::Schema.define(version: 20161223165054) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "email"
-    t.string "password"
-    t.string "password_confirmation"
+    t.string   "username"
+    t.string   "email"
+    t.string   "password"
+    t.string   "password_confirmation"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
   end
 
 end
