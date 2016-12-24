41c43fb37346d81773abc23bc7f4478843c9effc
Jeffrey Chang <jeffreycj.chang@gmail.com>
2016-12-24 19:57:07 +0000

    Users can now register and get saved into the database

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,11 @@
 class UsersController < ApplicationController
 
   def create
-    @user = User.new(user_params)
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(
-      :username,
-      :email,
-      :password,
-      :password_confirmation
+    @user = User.create(
+      username: params[:username],
+      email: params[:email],
+      password: params[:password],
+      password_confirmation: params[:password_confirmation]
     )
   end
 end
