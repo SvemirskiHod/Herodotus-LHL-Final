5343bc1697ebe66a119fa614753c9934d7131d3f
SvemirskiHod <joaocoelho93@gmail.com>
2016-12-26 17:13:33 +0000

    continuity commit

--- app/controllers/adv_searches_controller.rb
+++ app/controllers/adv_searches_controller.rb
@@ -9,16 +9,20 @@ class AdvSearchesController < ApplicationController
     location = params["location"]
     era = params["era"]
 
-
-
     @results = Movie.all
     @results = @results.where("lower(title) LIKE ?", "%#{title.downcase}%").all if title != ""
     @results = @results.where("lower(genre) LIKE ?", "%#{genre.downcase}%").all if genre != ""
-    @results = @results.where("set_start_year = #{set_start_year}").all if set_start_year != ""
+    @results = @results.where("lower(setting_location) LIKE ?", "%#{location.downcase}%").all if location != ""
     @results = @results.where("start_ad_bc = '#{era}'").all if era != ""
-    @results = @results.where("lower(setting_location) LIKE ?", "%#{location.downcase}").all if location != ""
-    movies = []
 
+
+    if (era.downcase == 'bc')
+      @results = @results.where("set_start_year = #{set_start_year}").all if set_start_year != ""
+    else
+        @movies = @movies.where("set_start_year >= ?", set_start_year.to_i).where("set_end_year <= ?", set_start_year.to_i).all
+    end
+
+    movies = []
     @results.each do |film|
       keywords = JSON.parse(film["keywords"])
       if (user_submitted_keywords - keywords).empty?
