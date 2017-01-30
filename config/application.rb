1a00ee590d7ad05c61d2e6372b6f1d2a9ebf244c
Jebbie87 <jeffreycj.chang@gmail.com>
2017-01-30 10:52:18 -0500

    Changed Rackcors back to heroku

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Herodotus
     # config.web_console.whitelisted_ips = '10.0.2.2'
     config.middleware.insert_before 0, Rack::Cors do
       allow do
-        origins 'https://herodotus-final.herokuapp.com'
+        origins '*'
         resource '*', :headers => :any, :methods => [:get, :post, :delete, :put, :options]
       end
     end
